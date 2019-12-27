f6c020f835c6f3efadd51b167c0f9729b03b28ac
John Reiner <johnreiner1990@gmail.com>
2019-12-27 02:11:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,10 +23,11 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if hash[key]
-    hash[:key] + 1
-  else
-    hash[key] =  1
+  def update_counting_hash(hash, key)
+    if hash[key]
+      hash[key] += 1
+    else
+      hash[key] =  1
+    end
+    return hash
   end
-  return hash
-end
