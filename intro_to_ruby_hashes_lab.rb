1995fdb3f8645681f001ed1adb39c128daee171c
John Reiner <johnreiner1990@gmail.com>
2019-12-27 02:10:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,8 +24,9 @@ end
 
 def update_counting_hash(hash, key)
   if hash[key]
-    key.to_sym += 1
+    hash[:key] += 1
   else
-    hash[key.to_sym] =  1
+    hash[key] =  1
   end
+  return hash
 end
